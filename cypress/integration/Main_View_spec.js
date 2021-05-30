677028beb490cdeac466d8de49f4aba55f5da337
Angela Battillo <angela.battillo@gmail.com>
2021-05-30 14:55:53 -0600

    Create test for back ground image display

--- cypress/integration/Main_View_spec.js
+++ cypress/integration/Main_View_spec.js
@@ -11,18 +11,25 @@ describe('Show main view of Worm Hole App', () => {
       })
 
     cy.visit('http://localhost:3000')
-  })
+  });
 
   it('Should be able to visit the main page', () => {
     cy.url().should('eq', 'http://localhost:3000/')
-  })
+  });
 
   it('Should display title on main page view', () => {
     cy.get('.glow').should('contain', 'W o r m h o l e')
-  })
+  });
 
-  it('Should show a randomization of countries selected button on load', () => {
+  it('Should show go home and randomization of countries selected buttons on load', () => {
     cy.get('.random-location').should('contain', 'Randomize Music Location')
-  })
+
+      .get('.go-home').should('contain', 'Return Home')
+  });
+
+  it('Should display background image inside the header of the main page', () => {
+    cy.get('header').should('have.css', 'background-image', 'url("http://localhost:3000/static/media/night-sky.3ce7405c.jpeg")')
+  });
+
 
 })
