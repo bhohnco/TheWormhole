060cf1e8c2a753e7493619d10d93f3249cbfa8a5
pcmueller <pmuellerleile@gmail.com>
2021-05-30 17:27:26 -0600

    Refactor/reformat TopArtists data & display

--- src/components/TopArtists/TopArtists.js
+++ src/components/TopArtists/TopArtists.js
@@ -2,26 +2,29 @@ import React from 'react';
 
 const TopArtists = ({topArtists}) => {
 
-  const artistNames = topArtists.topartists.artist.map(elem => {
-    return elem.name;
+  const filteredArtists = topArtists.topartists.artist.reduce((topTen, elem) => {
+    if (topArtists.topartists.artist.indexOf(elem) < 10) {
+      topTen.push(elem);
+    }
+    return topTen;
+  }, []);
+
+  const artistCards = filteredArtists.map(artist => {
+    return (
+        <p id={artist.mbid} key={artist.mbid} className='top-artist-card'>
+          <h3>{artist.name}</h3>
+          <img alt='artist-portrait'></img>
+        </p>
+    )
   });
+
+  console.log(artistCards);
 
   return (
     <article className='top-artists-box'>
       <h3> Top Artists in *location* </h3>
       <div className='artists-list'>
-        <ol>
-          <li className='top-artist'>{artistNames[0]}</li>
-          <li className='top-artist'>{artistNames[1]}</li>
-          <li className='top-artist'>{artistNames[2]}</li>
-          <li className='top-artist'>{artistNames[3]}</li>
-          <li className='top-artist'>{artistNames[4]}</li>
-          <li className='top-artist'>{artistNames[5]}</li>
-          <li className='top-artist'>{artistNames[6]}</li>
-          <li className='top-artist'>{artistNames[7]}</li>
-          <li className='top-artist'>{artistNames[8]}</li>
-          <li className='top-artist'>{artistNames[9]}</li>
-        </ol>
+        {artistCards}
       </div>
     </article>
   )
