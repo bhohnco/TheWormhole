25e5636b65e1730dd5b70c8e2ca5b43bb6a02224
Angela Battillo <angela.battillo@gmail.com>
2021-05-30 15:54:05 -0600

    Remove repetitious code to DRY up our app

--- src/components/TopTracks/TopTracks.js
+++ src/components/TopTracks/TopTracks.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
 const TopTracks = () => {
+  // const trackFind = topTracks.tracks.reduce((finalArray, currentArtist) => {
+  //   finalArray.push({name: currentArtist.artist.name, title: currentArtist.name});
+  //   return finalArray
+  // },[])
   return (
     <article className='top-tracks-box'>
       <h3> Top Tracks in *location* </h3>
       <div className='tracks-list'>
-        <ol>
-          <li className='top-track'>Track #1</li>
-          <li className='top-track'>Track #2</li>
-          <li className='top-track'>Track #3</li>
-          <li className='top-track'>Track #4</li>
-          <li className='top-track'>Track #5</li>
-          <li className='top-track'>Track #6</li>
-          <li className='top-track'>Track #7</li>
-          <li className='top-track'>Track #8</li>
-          <li className='top-track'>Track #9</li>
-          <li className='top-track'>Track #10</li>
+        <ol className='top-track'>
+          <li >Track #1</li>
+          <li >Track #2</li>
+          <li >Track #3</li>
+          <li >Track #4</li>
+          <li >Track #5</li>
+          <li >Track #6</li>
+          <li >Track #7</li>
+          <li >Track #8</li>
+          <li >Track #9</li>
+          <li >Track #10</li>
         </ol>
       </div>
     </article>
