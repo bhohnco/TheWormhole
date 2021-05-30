c5f6d59e5121ea57ae7e75d473d27b86ea3119a2
Angela Battillo <angela.battillo@gmail.com>
2021-05-30 17:13:37 -0600

    Remove repetitive code for DRY principle

--- src/components/TopArtists/TopArtists.js
+++ src/components/TopArtists/TopArtists.js
@@ -11,17 +11,17 @@ const TopArtists = ({ topArtists }) => {
     <article className='top-artists-box'>
       <h3> Top Artists in *location* </h3>
       <div className='artists-list'>
-        <ol>
-          <li className='top-artist'>{artistNames[0]}</li>
-          <li className='top-artist'>{artistNames[1]}</li>
-          <li className='top-artist'>{artistNames[2]}</li>
-          <li className='top-artist'>{artistNames[3]}</li>
-          <li className='top-artist'>{artistNames[4]}</li>
-          <li className='top-artist'>{artistNames[5]}</li>
-          <li className='top-artist'>{artistNames[6]}</li>
-          <li className='top-artist'>{artistNames[7]}</li>
-          <li className='top-artist'>{artistNames[8]}</li>
-          <li className='top-artist'>{artistNames[9]}1</li>
+        <ol className='top-artist'>
+          <li >{artistNames[0]}</li>
+          <li >{artistNames[1]}</li>
+          <li >{artistNames[2]}</li>
+          <li >{artistNames[3]}</li>
+          <li >{artistNames[4]}</li>
+          <li >{artistNames[5]}</li>
+          <li >{artistNames[6]}</li>
+          <li >{artistNames[7]}</li>
+          <li >{artistNames[8]}</li>
+          <li >{artistNames[9]}1</li>
         </ol>
       </div>
     </article>
