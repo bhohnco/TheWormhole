a488479307c87e9e17ff319e7c8964461437ddb3
pcmueller <pmuellerleile@gmail.com>
2021-05-29 19:30:00 -0600

    Build basic component HTML and CSS

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,40 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Form from '../Form/Form';
+import TopArtists from '../TopArtists/TopArtists';
+import TopTracks from '../TopTracks/TopTracks';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      nation: '',
+      topArtists: '',
+      topTracks: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Form />
+        <main className='main-section'>
+          <section className='nation-display'>
+            <TopArtists />
+            <TopTracks />
+          </section>
+          {/*
+          <section className='artist-display'>
+            <ArtistInfo />
+            <ArtistTracks />
+          </section>
+          */}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Form.css';
+
+const Form = () => {
+
+  return (
+    <div>
+      <form>
+        <label> Select Country
+          <select>DROP DOWN</select>
+        </label>
+        <label> Search for artist
+          <input />
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default Form;
