566def131f3201a6e6cdac0fde13db635381c622
Angela Battillo <angela.battillo@gmail.com>
2021-05-30 12:07:29 -0600

    Indent for dev empathy

--- cypress/fixtures/mock-topArtist-stubbing-data.js
+++ cypress/fixtures/mock-topArtist-stubbing-data.js
@@ -28,7 +28,7 @@
       "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
       "size": "mega"
       }
-    ]
+      ]
     },
     {
     "name": "Radiohead",
@@ -36,28 +36,28 @@
     "mbid": "a74b1b7f-71a5-4011-9441-d0b5e4122711",
     "url": "https://www.last.fm/music/Radiohead",
     "streamable": "0",
-    "image": [
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "small"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "medium"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "large"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "extralarge"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "mega"
-    }
-    ]
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+      ]
     },
     {
     "name": "Fleetwood Mac",
@@ -65,28 +65,28 @@
     "mbid": "bd13909f-1c29-4c27-a874-d4aaf27c5b1a",
     "url": "https://www.last.fm/music/Fleetwood+Mac",
     "streamable": "0",
-    "image": [
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "small"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "medium"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "large"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "extralarge"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "mega"
-    }
-    ]
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+      ]
     },
     {
     "name": "The Beatles",
@@ -94,28 +94,28 @@
     "mbid": "b10bbbfc-cf9e-42e0-be17-e2c3e1d2600d",
     "url": "https://www.last.fm/music/The+Beatles",
     "streamable": "0",
-    "image": [
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "small"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "medium"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "large"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "extralarge"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "mega"
-    }
-    ]
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+      ]
     },
     {
     "name": "Queen",
@@ -123,28 +123,28 @@
     "mbid": "420ca290-76c5-41af-999e-564d7c71f1a7",
     "url": "https://www.last.fm/music/Queen",
     "streamable": "0",
-    "image": [
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "small"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "medium"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "large"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "extralarge"
-    },
-    {
-    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-    "size": "mega"
-    }
-    ]
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+      ]
     },
     {
     "name": "Coldplay",
@@ -152,6 +152,64 @@
     "mbid": "cc197bad-dc9c-440d-a5b5-d52ba2e14234",
     "url": "https://www.last.fm/music/Coldplay",
     "streamable": "0",
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+      ]
+    },
+    {
+    "name": "The Rolling Stones",
+    "listeners": "4116467",
+    "mbid": "b071f9fa-14b0-4217-8e97-eb41da73f598",
+    "url": "https://www.last.fm/music/The+Rolling+Stones",
+    "streamable": "0",
+      "image": [
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "small"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "medium"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "large"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "extralarge"
+      },
+      {
+      "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+      "size": "mega"
+      }
+    ]
+  },
+  {
+  "name": "Daft Punk",
+  "listeners": "4162369",
+  "mbid": "056e4f3e-d505-4dad-8ec1-d04f521cbb56",
+  "url": "https://www.last.fm/music/Daft+Punk",
+  "streamable": "0",
     "image": [
     {
     "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
@@ -174,13 +232,13 @@
     "size": "mega"
     }
     ]
-    },
-    {
-    "name": "The Rolling Stones",
-    "listeners": "4116467",
-    "mbid": "b071f9fa-14b0-4217-8e97-eb41da73f598",
-    "url": "https://www.last.fm/music/The+Rolling+Stones",
-    "streamable": "0",
+  },
+  {
+  "name": "The Cure",
+  "listeners": "3254675",
+  "mbid": "69ee3720-a7cb-4402-b48d-a02c366f2bcf",
+  "url": "https://www.last.fm/music/The+Cure",
+  "streamable": "0",
     "image": [
     {
     "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
@@ -202,65 +260,7 @@
     "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
     "size": "mega"
     }
-  ]
-  },
-  {
-  "name": "Daft Punk",
-  "listeners": "4162369",
-  "mbid": "056e4f3e-d505-4dad-8ec1-d04f521cbb56",
-  "url": "https://www.last.fm/music/Daft+Punk",
-  "streamable": "0",
-  "image": [
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "small"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "medium"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "large"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "extralarge"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "mega"
-  }
-  ]
-  },
-  {
-  "name": "The Cure",
-  "listeners": "3254675",
-  "mbid": "69ee3720-a7cb-4402-b48d-a02c366f2bcf",
-  "url": "https://www.last.fm/music/The+Cure",
-  "streamable": "0",
-  "image": [
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "small"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "medium"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "large"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "extralarge"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "mega"
-  }
-  ]
+    ]
   },
   {
   "name": "Nirvana",
@@ -268,27 +268,28 @@
   "mbid": "9282c8b4-ca0b-4c6b-b7e3-4f7762dfc4d6",
   "url": "https://www.last.fm/music/Nirvana",
   "streamable": "0",
-  "image": [
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "small"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "medium"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "large"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "extralarge"
-  },
-  {
-  "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
-  "size": "mega"
+    "image": [
+    {
+    "#text": "https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png",
+    "size": "small"
+    },
+    {
+    "#text": "https://lastfm.freetls.fastly.net/i/u/64s/2a96cbd8b46e442fc41c2b86b821562f.png",
+    "size": "medium"
+    },
+    {
+    "#text": "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png",
+    "size": "large"
+    },
+    {
+    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+    "size": "extralarge"
+    },
+    {
+    "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png",
+    "size": "mega"
+    }
+    ]
   }
-]
 }
 }
