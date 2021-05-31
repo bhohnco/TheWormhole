5fae06fbab86dd8588bf11a73efc65af15d228ab
Angela Battillo <angela.battillo@gmail.com>
2021-05-31 10:12:00 -0600

    Allow import for error handler

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,3 +1,5 @@
+import { checkForErr } from './utils.js'
+
 const topArtistByCountryUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=ireland&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json"
 //"http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country={country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json"
 const topTracksByCountryUrl = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=ireland&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json"
@@ -10,16 +12,20 @@ const artistImageUrl =  "http://musicbrainz.org/ws/2/artist/5441c29d-3602-4898-b
 
 export const apiCalls = {
 
-  async getTopArtists() {
+  async getTopArtists () {
     const response = await fetch(topArtistByCountryUrl)
+                           .then(checkForErr)
     const data = await response.json();
-    return data;
+    return data
+
   },
 
-  async getTopTracks() {
-    const response = await fetch(topTracksByCountryUrl);
-    const data = await response.json();
-    return data;
+  async getTopTracks () {
+    const response = await fetch(topTracksByCountryUrl)
+                           .then(checkForErr)
+    const data = await response.json()
+    return data
+
   },
 
   // async getSelectedArtist(id) {
@@ -35,15 +41,7 @@ export const apiCalls = {
   // },
 
 
-//   export const checkForErr = (response) => {
-//   if(response.status >= 500) {
-//     return 'Uhoh! Something is wrong with our system. Please try back later.'
-//   } else if (!response.ok) {
-//     return 'Something went wrong. Please try again later.'
-//   } else {
-//     return response.json()
-//   }
-// }
+//   export
 
   //          throw new Error(`Sorry, we are having trouble getting the Top Artists, please try again later.`)
 //  throw new Error(`Sorry, we are having trouble getting the Top Tracks, please try again later.`)
