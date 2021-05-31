f29b49284f7a8f6ef5d2e65fdb762c8f29b82b96
Angela Battillo <angela.battillo@gmail.com>
2021-05-31 10:11:23 -0600

    Create a 404 specific error message

---
+++ src/utilities/utils.js
@@ -0,0 +1,11 @@
+export const checkForErr = (response) => {
+    if(response.status >= 500) {
+      return '500: Uhoh, the angry computer gnome just drop kicked one of our servers. This page is temporarily unavailable.'
+    } else if(response.status === 404) {
+      return '404: oh no! Nothing in here. We cannot seem to find the page you are looking for.'
+    } else if (!response.ok) {
+      return 'Something small went wrong. Please try again later.'
+    } else {
+      return response.json()
+    }
+}
