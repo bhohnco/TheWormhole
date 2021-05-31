93944b5f2d64ad1c1bff9f4b4cc35bbe026482b6
bhohnco <bryanhohn13@gmail.com>
2021-05-31 10:44:25 -0600

    Redux files add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
+// REDUX
+// import { Provider } from 'react-redux';
+// import store from './redux/store';
 
 ReactDOM.render(<App /> , document.getElementById('root')
 );
