2ade7f1ec40283e3fd60170a44b02b076387a8b5
bhohnco <bryanhohn13@gmail.com>
2021-05-31 14:28:15 -0600

    Adjust to updated file

---
+++ src/actions/actions.js
@@ -0,0 +1,18 @@
+
+export const actionGetTopArtist = (data) => ({
+  return {
+    type: 'GET_TOP_ARTIST_DATA',
+    payload: data,
+})
+
+export const actionGetTopTracks = (data) => ({
+  return {
+    type: 'GET_TOP_TRACKS_DATA',
+    payload: data,
+  })
+
+export const hasError = (message) => ({
+return {
+    type: 'HAS_ERROR'
+}
+})
