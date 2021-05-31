95fee9489cf25c58a90c788f76941f10af3d58a3
Angela Battillo <angela.battillo@gmail.com>
2021-05-31 10:29:18 -0600

    Remove repetitious unused code per DRY

--- src/components/ArtistTracks/ArtistTracks.js
+++ src/components/ArtistTracks/ArtistTracks.js
@@ -5,17 +5,17 @@ const ArtistTracks = () => {
     <article className='artist-tracks-box'>
       <h3> Top Tracks by *artist* </h3>
       <div className='artist-tracks-list'>
-        <ol>
-          <li className='artist-track'>Track #1</li>
-          <li className='artist-track'>Track #2</li>
-          <li className='artist-track'>Track #3</li>
-          <li className='artist-track'>Track #4</li>
-          <li className='artist-track'>Track #5</li>
-          <li className='artist-track'>Track #6</li>
-          <li className='artist-track'>Track #7</li>
-          <li className='artist-track'>Track #8</li>
-          <li className='artist-track'>Track #9</li>
-          <li className='artist-track'>Track #10</li>
+        <ol className='artist-track'>
+          <li >Track #1</li>
+          <li >Track #2</li>
+          <li >Track #3</li>
+          <li >Track #4</li>
+          <li >Track #5</li>
+          <li >Track #6</li>
+          <li >Track #7</li>
+          <li >Track #8</li>
+          <li >Track #9</li>
+          <li >Track #10</li>
         </ol>
       </div>
     </article>
