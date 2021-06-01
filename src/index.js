9077b4da41850a11e657eb970db1d8c3b60f4194
bhohnco <bryanhohn13@gmail.com>
2021-06-01 11:19:17 -0600

    Add provider and root reducer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.scss';
-
 // REDUX
+import thunk from 'redux-thunk'
 import rootReducer from './reducers/rootReducer';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
@@ -14,6 +14,8 @@ const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk
 ReactDOM.render(
     <Provider store={store}>
     <App />
-    </Provider>
+    </Provider>,
     document.getElementById('root')
 );
+
+export default store;
