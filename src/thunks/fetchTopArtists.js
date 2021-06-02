bdb10584ddd3a8164d792257b2224b3b45fd289b
Angela Battillo <angela.battillo@gmail.com>
2021-06-02 10:51:47 -0600

    Shift syntax for testing

--- src/thunks/fetchTopArtists.js
+++ src/thunks/fetchTopArtists.js
@@ -3,23 +3,25 @@ import { isLoading, hasErrored, actionGetTopArtists } from '../actions/index'
 export const fetchTopArtists = () => {
   console.log("am i here")
   const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=ireland&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`
+
   return dispatch => {
     dispatch({
       type: 'GET_TOP_ARTIST_DATA',
       isLoading
     })
     //then we wanna return this block here
-   //
-  return
-    try {
-      const response = fetch(url)
-      if(!response.ok) {
-        throw Error(response.statusText)
-      }
-      const data = await response.json()
-      dispatch(actionGetTopArtists(data))
+    //
+    return async (dispatch) => {
+      try {
+        const response = fetch(url)
+        if (!response.ok) {
+          throw Error(response.statusText)
+        }
+        const data = response.json()
+        dispatch(actionGetTopArtists(data))
       } catch (error) {
-      dispatch(hasErrored(error.message))
+        dispatch(hasErrored(error.message))
+      }
     }
   }
 }
