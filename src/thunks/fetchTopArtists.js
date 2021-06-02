e58fde77a62299d1f1228123a92abfaa1caa3a2c
Angela Battillo <angela.battillo@gmail.com>
2021-06-01 22:16:32 -0600

    "Start two returns for proper first use of dispatch before fetch per thunks"

--- src/thunks/fetchTopArtists.js
+++ src/thunks/fetchTopArtists.js
@@ -1,20 +1,25 @@
 import { isLoading, hasErrored, actionGetTopArtists } from '../actions/index'
 
-console.log("fetchTopArtist")
 export const fetchTopArtists = () => {
-  console.log("fetchTopArtist")
+  console.log("am i here")
   const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=ireland&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`
-  return async (dispatch) =>  {
+  return dispatch => {
+    dispatch({
+      type: 'GET_TOP_ARTIST_DATA',
+      isLoading
+    })
+    //then we wanna return this block here
+   //
+  return
     try {
-      const response = await fetch(url)
+      const response = fetch(url)
       if(!response.ok) {
         throw Error(response.statusText)
       }
       const data = await response.json()
       dispatch(actionGetTopArtists(data))
-    } catch (error) {
+      } catch (error) {
       dispatch(hasErrored(error.message))
     }
   }
 }
-
