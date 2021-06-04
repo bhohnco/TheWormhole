189457f540e7af7136d221f6b65ede987d505ffc
bhohnco <bryanhohn13@gmail.com>
2021-06-04 17:22:49 -0600

    Form adjust in render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,11 @@ import { location } from '../../actions';
 import utils from '../../utilities/utils';
 import Background from '../../assets/images/black-cloth.jpeg';
 import Header from '../Header/Header';
-import DropDown from '../Dropdown/Dropdown';
 import Form from '../Form/Form';
 import TopArtists from '../TopArtists/TopArtists';
 import TopTracks from '../TopTracks/TopTracks';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> c138ae6cb614229990b020f4017341c286f41d62
 const App = () => {
 
   const dispatch = useDispatch();
@@ -31,11 +27,7 @@ const App = () => {
       </main>
     )
   } else {
-<<<<<<< HEAD
     console.log("LOCATION LOADED: ", locationObj);
-
-=======
->>>>>>> c138ae6cb614229990b020f4017341c286f41d62
       return (
         <div className="App" style={{ backgroundImage: `url(${Background})`}}>
           <Header />
@@ -50,43 +42,8 @@ const App = () => {
         </div>
       );
   }
-<<<<<<< HEAD
-  
-  // const mapStateToProps = ({ setTopArtists }) => ({
-  //   topArtists: setTopArtists,
-  // })
-  //
-  // export const mapDispatchToProps = dispatch => (
-  //   bindActionCreators({
-  //     hasErrored,
-  //     isLoading,
-  //     actionGetTopArtists
-  //   },
-  //   dispatch)
-  // )
-
-
-
-=======
->>>>>>> c138ae6cb614229990b020f4017341c286f41d62
 }
 
+
 export default App;
-
-
-  
-  // const mapStateToProps = ( state ) => ({
-  //   topArtists: state.artists,
-  //   isLoading: state.isLoading,
-  //   // error: state.error,
-  // })
-
-
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-  // const mapDispatchToProps = (dispatch) => ({
-  //   topArtists: () => dispatch(fetchTopArtists())
-  // });
-  
-  // export default connect(mapStateToProps, mapDispatchToProps)(App);
 
