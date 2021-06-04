b1f428eb56f3446e84cd75ea5cdd5e6e3d369ad9
bhohnco <bryanhohn13@gmail.com>
2021-06-04 08:23:41 -0600

    Adjusted form layout and add dropdown

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import Dropdown from '../Dropdown/Dropdown';
-import countries from '../../utilities/countries';
+import Dropdown from '../Dropdown/Dropdown'
+import countries from '../../utilities/countries'
 
-const Form = (props) => {
-  const { handleSubmit } = props;
+const Form = () => {
+
   return (
     <div>
-      <form className='search-form'onSubmit={handleSubmit}>
-        <label> Select Country
-          <select>
-            <Field name='country-select' component={Dropdown} options={userOptions} />
-          </select>
+      <form className='search-form'>
+        <label> 
+          <Dropdown title="Select Country" items={countries} />
         </label>
         <button>Search Artist Library
           <input type='text' placeholder='type in names of artists'/>
