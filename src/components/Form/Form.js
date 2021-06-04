ac78d1b187c75dc1540916985939b61ad5b4a08f
bhohnco <bryanhohn13@gmail.com>
2021-06-04 17:21:13 -0600

    Form adjusted with dropdown add

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Dropdown from '../Dropdown/Dropdown'
 import countries from '../../utilities/countries'
 
 const Form = () => {
 
+  const [value, setValue] = useState(null)
+
   return (
     <div>
       <form className='search-form'>
-        <label> 
-          <Dropdown title="Select Country" items={countries} />
-        </label>
+        <div style ={{ width: 220}}>
+          <Dropdown prompt='Select country...'
+                    options={countries}
+                    value={value}
+                    onChange={val => setValue(val) }
+          />
+        </div>
         <button>Search Artist Library
           <input type='text' placeholder='type in names of artists'/>
         </button>
