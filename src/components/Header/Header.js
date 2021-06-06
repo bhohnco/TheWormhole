523dd4f3017e2b7b4e87268fe89eeebd9148c259
Angela Battillo <angela.battillo@gmail.com>
2021-06-05 23:20:15 -0600

    Change button words to fit screen shifts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <h1 className='glow'>W o r m h o l e</h1>
       <nav>
         <Link exact to='/'>
-          <button className='random-location'>Randomize Location</button>
+          <button className='random-location'>Randomize Me</button>
         </Link>
         <Link exact to='/'>
           <button className='go-home'>Return Home</button>
