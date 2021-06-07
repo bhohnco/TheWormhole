93740f64ff2c1297d781bc28d052e845ffe50091
bhohnco <bryanhohn13@gmail.com>
2021-06-06 21:15:24 -0600

    Search bar function close to working

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,45 +1,79 @@
 import React, { Component } from 'react';
 import { searchData } from '../../actions/index';
+import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-
+import { withRouter } from 'react-router-dom';
 class SearchBar extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchWord: ''
-    }
+
+  handleFormSubmit = function({query}) {
+    console.log('trying to handle subimit for query', query);
   }
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
+  renderInput(field) {
+    return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.searchData(this.state.searchWord)
-    this.clearInputs();
-  }
-
-  clearInputs = () => {
-    this.setState({ searchWord: '' })
-  }
-//   to trigger pete's styles of the second search bar styles folder <div className='search-container'>
   render() {
+
+    const { handleSubmit } = this.props;
+
     return (
-        <section className='search-bar-box' >
-          <input
-              className='search-input'
-              type='text'
-              placeholder='Search artists...'
-              id='searchWord'
-              name='searchWord'
-              value={this.state.searchWord}
-              onChange={this.handleChange}
-          />
-          <button className='search-btn' onClick={this.handleSubmit}>Search</button>
-        </section>
-    )
+        <section className={`search-bar search-bar__${this.props.page}`} onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+          <div className="search-bar__wrapper">
+          <Field name="artist:" component={this.renderInput}/>
+          <p>Press return to search</p>
+        </div>
+  </section>
+  )
   }
 }
 
-export default connect(null, {searchData})(SearchBar)
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchWord: ''
+//     }
+//   }
+//
+//   handleChange = e => {
+//     this.setState({ [e.target.name]: e.target.value })
+//   }
+//
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.searchData(this.state.searchWord)
+//     this.clearInputs();
+//   }
+//
+//   clearInputs = () => {
+//     this.setState({ searchWord: '' })
+//   }
+// //   to trigger pete's styles of the second search bar styles folder <div className='search-container'>
+//   render() {
+//     return (
+//         <section className='search-bar-box' >
+//           <input
+//               className='search-input'
+//               type='text'
+//               placeholder='Search artists...'
+//               id='searchWord'
+//               name='searchWord'
+//               value={this.state.searchWord}
+//               onChange={this.handleChange}
+//           />
+//           <button className='search-btn' onClick={this.handleSubmit}>Search</button>
+//         </section>
+//     )
+//   }
+// }
+//
+// export default connect(null, {searchData})(SearchBar)
+
+
+SearchBar = reduxForm({
+  form: 'searchBar'
+})(SearchBar);
+
+SearchBar = withRouter(SearchBar);
+
+export default SearchBar;
