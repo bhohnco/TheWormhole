a3e1641acc6e78995957a1a5025dec18c88d0cb3
bhohnco <bryanhohn13@gmail.com>
2021-06-07 08:40:23 -0600

    utilities/utils.js

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -4,13 +4,12 @@ import utils from './utils'
 const apiCalls = {
 
   async getTopArtists(country) {
-    return await fetch(`http://ws.auioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+    return await fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
         .then(response => {
-          if (!response.ok) {
-            console.log(response)
-            utils.checkForErr(response)
-          } else {
+          if (response.ok) {
             return response.json()
+          } else {
+            utils.checkForErr(response)
           }
         })
   },
@@ -18,11 +17,10 @@ const apiCalls = {
   async getTopTracks(country) {
     return await fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
         .then(response => {
-          if (!response.ok) {
-            const message = `An error has occurred: ${response.status}`;
-            throw new Error(message);
-          } else {
+          if (response.ok) {
             return response.json()
+          } else {
+            utils.checkForErr(response)
           }
         })
   },
@@ -30,11 +28,10 @@ const apiCalls = {
   async getArtistImage(id) {
     return await fetch(`http://musicbrainz.org/ws/2/artist/${id}?inc=url-rels&fmt=json`)
         .then(response => {
-          if (!response.ok) {
-            const message = `An error has occurred: ${response.status}`;
-            throw new Error(message);
-          } else {
+          if (response.ok) {
             return response.json()
+          } else {
+            utils.checkForErr(response)
           }
         })
   },
@@ -42,24 +39,23 @@ const apiCalls = {
   async getSelectedCountry(country){
       const topArtistSelect = fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
           .then(response => {
-            if (!response.ok) {
-              const message = `An error has occurred: ${response.status}`;
-              throw new Error(message);
-            } else {
+            if (response.ok) {
               return response.json()
+            } else {
+              utils.checkForErr(response)
             }
           })
-
+  
 
       const topTrackSelect = fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
           .then(response => {
-            if (!response.ok) {
-              const message = `An error has occurred: ${response.status}`;
-              throw new Error(message);
-            } else {
+            if (response.ok) {
               return response.json()
+            } else {
+              utils.checkForErr(response)
             }
           })
+
 
 
       return Promise.all([topArtistSelect, topTrackSelect])
@@ -76,11 +72,10 @@ const apiCalls = {
   async getArtistInfo(artistName) {
     return await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
         .then(response => {
-          if (!response.ok) {
-            const message = `An error has occurred: ${response.status}`;
-            throw new Error(message);
-          } else {
+          if (response.ok) {
             return response.json()
+          } else {
+            utils.checkForErr(response)
           }
         })
   },
@@ -88,14 +83,13 @@ const apiCalls = {
   async getArtistImagePage(id) {
     return await fetch(`http://musicbrainz.org/ws/2/artist/${id}?inc=url-rels&fmt=json`)
         .then(response => {
-          if (!response.ok) {
-            const message = `An error has occurred: ${response.status}`;
-            throw new Error(message);
-          } else {
+          if (response.ok) {
             return response.json()
+          } else {
+            utils.checkForErr(response)
           }
         })
-  }
+  },
 
   // async getArtistImageURL() {
   //   const response = await fetch(`http://commons.wikimedia.org/wiki/File:ArianaGrandeDecember2013.jpg`)
