fce4021c3b670cd8a518bca61c5813558645cf98
bhohnco <bryanhohn13@gmail.com>
2021-06-07 14:49:24 -0600

    Removed unused code

--- src/actions/index.js
+++ src/actions/index.js
@@ -54,18 +54,6 @@ export const dropdownValues = (e, data) => {
   }
 }
 
-// export const dropdownValues = (selectedCountry) => {
-//         return (dispatch) => {
-//         fetch(`//http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${Artist+selectedArtist}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`,
-//             { headers: { "Accept": "application/json" }
-//         })
-//             .then(checkForError)
-//             .then(data => dispatch({ type: 'DROPDOWN_SELECT', payload: data }))
-//             .catch(error => alert(error))
-//     }
-// }
-
-
 export const searchData = (searchTerm) => {
     return (dispatch) => {
         fetch(`//http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Artist+${searchTerm}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json"`,
@@ -77,19 +65,6 @@ export const searchData = (searchTerm) => {
     }
 }
 
-// export function fetchPostsWithQuery(query, callback) {
-//     return function(dispatch) {
-//         axios.get(`https://api.dailysmarty.com/search?q=${query}`)
-//             .then(response => {
-//                 dispatch({
-//                     type: SET_RESULTS_POSTS,
-//                     payload: response.data.posts
-//                 })
-//                 if(callback) { callback() }
-//             })
-//     }
-// }
-
 const checkForError = (response) => {
     if (!response.ok) {
         throw new Error(response.message);
@@ -97,12 +72,3 @@ const checkForError = (response) => {
         return response.json();
     }
 }
-
-
-// export const searchForArtist = dispatch => ({
-//     onSearchTermChange: (value) =>
-//         dispatch({
-//             type: 'SEARCH_TERM_CHANGE',
-//             value
-//         })
-// })
