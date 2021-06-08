1b7c33763b21023f6e3a812e670384c943c1f285
pcmueller <pmuellerleile@gmail.com>
2021-06-07 19:00:12 -0600

    Set max artist bio height

--- src/components/TopArtists/TopArtists.js
+++ src/components/TopArtists/TopArtists.js
@@ -9,7 +9,6 @@ const TopArtists = () => {
   const dispatch = useDispatch();
 
   const [artistCards, setArtistCards] = useState([]);
-  // const [artistImages, setArtistImages] = useState([]);
 
   const location = useSelector(state => state.location);
   const topArtists = useSelector(state => state.topArtists);
@@ -37,29 +36,9 @@ const TopArtists = () => {
     }
   }
 
-  // const fetchArtistImage = async (id) => {
-  //   const imageObj = await apiCalls.getArtistImagePage(id);
-  //   const pageURL = await locateImagePageURL(imageObj);
-  //   artistImages.push(pageURL || undefined);
-  // }
-
-  // const locateImagePageURL = (imageObj) => {
-  //   const imageKeys = Object.keys(imageObj.relations);
-  //   let imageURL = '';
-
-  //   imageKeys.forEach(key => {
-  //     if (imageObj.relations[key].type === 'image') {
-  //       imageURL = imageObj.relations[key].url.resource;
-  //     }
-  //   });
-
-  //   return imageURL;
-  // }
-
   const filterArtists = (data) => {
     const topArtists = data.reduce((topTen, artistObj) => {
       if (data.indexOf(artistObj) < 12) {
-        // fetchArtistImage(artistObj.mbid);
         topTen.push(artistObj);
       }
       return topTen;
@@ -70,13 +49,12 @@ const TopArtists = () => {
   const buildCards = (topArtists) => topArtists.map(artist => {
 
     let nameString = artist.name.replaceAll(' ', '+');
-    // let imgURL = artistImages[topArtists.indexOf(artist)];
 
     return (
       <article id={artist.mbid} key={artist.mbid} className='top-artist-card'>
         <p>{artist.name}</p>
         <Link to={`/artist:${nameString}`} id={nameString} className='link-container'>
-          <img alt='artist-portrait'></img>
+          <img src='' alt='artist-portrait'></img>
         </Link>
       </article>
     )
