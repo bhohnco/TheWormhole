efaef8bd9374ec1f5c796247aeec2c7ff8b7bf49
Angela Battillo <angela.battillo@gmail.com>
2021-06-07 20:20:44 -0600

    Refactor api call for no es5 in a react project

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,40 +1,35 @@
 // import { useSelector, useDispatch } from 'react-redux';
 
-const apiCalls = {
+// const apiCalls = {
+export const getTopArtists = (country) => {
+    return fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+    .then(response => response.json());
+  }
 
-  async getTopArtists(country) {
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
-    const data = await response.json();
+export const getTopTracks = (country) => {
+    return fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+    .then(response => response.json());
 
-    return data;
-  },
+  }
 
-  async getTopTracks(country) {
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
-    const data = await response.json();
+export const getArtistInfo = (artistName) => {
+    return fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+    .then(response => response.json());
 
-    return data;
-  },
+  }
 
-  async getArtistInfo(artistName) {
-    const response = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
-    const data = await response.json();
-    
-    return data;
-  },
-
-  async getArtistImagePage(id) {
+  export const getArtistImagePage = async ( id) => {
     const response = await fetch(`http://musicbrainz.org/ws/2/artist/${id}?inc=url-rels&fmt=json`)
     const data = await response.json();
 
     return data;
-  },
+  }
 
   // async getArtistImageURL() {
   //   const response = await fetch(`http://commons.wikimedia.org/wiki/File:ArianaGrandeDecember2013.jpg`)
   //   const data = await response.json();
   //   console.log(data);
   // }
-}
+//}
 
-export default apiCalls;
+//export default apiCalls;
