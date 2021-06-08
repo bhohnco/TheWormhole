84d3746affb03cf5294be198e45c095bc642ddba
bhohnco <bryanhohn13@gmail.com>
2021-06-07 19:13:59 -0600

    API grab for artist search page

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,44 +1,61 @@
 import React, { Component, useState } from 'react';
-import { searchData } from '../../actions/index';
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
+import apicalls from '../../utilities/apiCalls'
+import utils from '../../utilities/utils'
 import { Link } from 'react-router-dom';
+
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchQuery: ''
+      searchQuery: '',
+      isValid: false
     }
-      this.handleChange = this.handleChange.bind(this);
-    }
-
-  handleChange(event) {
-      this.setState({
-        searchQuery: event.target.value
-      });
-    }
-
-
-    render()
-    {
-      return (
-          // <Link to={`/artist:${searchQuery}`} id={searchQuery}>
-          <form className='search-bar-box'>
-            <input
-                className='search-input'
-                type='text'
-                placeholder='Search Artists'
-                name='input'
-                value={this.state.searchQuery} onChange={this.handleChange}/>
-            <Link to={`/artist:${this.state.searchQuery}`}>
-              <button className='search-btn'>Search</button>
-            </Link>
-          </form>
-
-      )
-    }
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount = async (searchQuery) => {
+    await apicalls.getArtistInfo(searchQuery)
+        .then(response => {
+          if(response.ok)
+          this.setState({isValid: true})
+        })
+  }
+
+  handleChange(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+  renderSearchError = () => {
+    return (
+        <article className="display-error">
+          <h4 className="error-message">We couldn't find that artist, please try again!</h4>
+        </article>
+    )
+  }
+
+  render() {
+    return (
+        <section className='search-bar-box'>
+        <Link to={`/artist:${this.state.searchQuery}`}>
+          render={() => (
+            !this.state.isValid ?
+          <input
+              className='search-input'
+              type='text'
+              placeholder='Search Artists'
+              name='input'
+              value={this.state.searchQuery} 
+              onChange={this.handleChange}
+              renderSearchError={this.renderSearchError}/>: this.renderError()
+          )}/>
+          <button className='search-btn'>Search</button>
+          </Link>
+        </section>
+    )
+  }
+}
 
 export default SearchBar;
