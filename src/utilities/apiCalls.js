f4086d20c2733c56b9d1a791c7801977958f3c6a
pcmueller <pmuellerleile@gmail.com>
2021-07-01 15:54:08 -0500

    Switch API requests to HTTPS

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,15 +1,15 @@
 export const getTopArtists = (country) => {
-  return fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+  return fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
     .then(response => response.json());
 }
 
 export const getTopTracks = (country) => {
-  return fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+  return fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${country}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
     .then(response => response.json());
 }
 
 export const getArtistInfo = (artistName) => {
-  return fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
+  return fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
     .then(response => response.json());
 }
 
