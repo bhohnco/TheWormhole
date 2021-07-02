ff3414425558d652ae0484ef2ac63c25a3b6e28f
pcmueller <pmuellerleile@gmail.com>
2021-07-01 19:19:23 -0500

    feature/remove-thunk-import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import allReducers from './reducers/rootReducer';
-import thunk from 'redux-thunk';
 const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
