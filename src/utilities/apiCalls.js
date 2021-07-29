fe113d3fab340db4482e53e1de8943a8ed883259
pcmueller <pmuellerleile@gmail.com>
2021-07-29 11:22:45 -0600

    Added Top10 US artist image links

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -12,10 +12,3 @@ export const getArtistInfo = (artistName) => {
   return fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=18f07debe7c3cfc543178cd9046e1ec4&format=json`)
     .then(response => response.json());
 }
-
-// export const getArtistImage = async (url) => {
-//   const response = await fetch(url)
-//   const data = await response.json();
-
-//     return data;
-// }
